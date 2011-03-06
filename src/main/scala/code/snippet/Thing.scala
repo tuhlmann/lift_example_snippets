f610ab0983f2c582f25d1063e6ae99eba1404bac
Torsten Uhlmann <tuhlmann@agynamix.de>
2011-03-06 13:57:58 +0100

    Got the counter example working

--- src/main/scala/code/snippet/Thing.scala
+++ src/main/scala/code/snippet/Thing.scala
@@ -9,23 +9,66 @@ import net.liftweb.common.Full
 import net.liftweb.http.{S, SessionVar, SHtml}
 import net.liftweb.http.js.JsCmds.SetHtml
 import xml.Text
+import net.liftweb._
+import http._
+import common._
+import util.Helpers._
+import js._
+import JsCmds._
+import JE._
+import scala.xml.NodeSeq
 
 class Thing1 {
 
   var cnt = 0
 
   def value = "* *" #> cnt
-  def button = "* [onsubmit]" #> SHtml.ajaxInvoke(() => {cnt += 1; SetHtml("count", Text(cnt.toString))})
+  def button = "* [onclick]" #> SHtml.ajaxInvoke(() => {cnt += 1; SetHtml("count1", Text(cnt.toString))})
 
 }
 
-class Thing2 {
+class Thing2 extends Logger {
 
   var cnt = 0
 
   def render = "*" #> SHtml.idMemoize {
     memo =>
-    "span *" #> cnt & "button [onsubmit]" #> SHtml.ajaxInvoke(() => {cnt += 1; memo.setHtml()})
+    "span *" #> cnt & "button [onclick]" #> SHtml.ajaxInvoke(() => {cnt += 1; memo.setHtml()})
   }
 
 }
+
+object Thing3 {
+
+  var cnt = 0
+
+  def value = "* *" #> cnt
+  def button = "* [onclick]" #> SHtml.ajaxInvoke(() => {cnt += 1; SetHtml("count3", Text(cnt.toString))})
+
+}
+
+object Thing4 {
+
+  var cnt = 0
+
+  def render = "*" #> SHtml.idMemoize {
+    memo =>
+    "span *" #> cnt & "button [onclick]" #> SHtml.ajaxInvoke(() => {cnt += 1; memo.setHtml()})
+  }
+}
+
+object ShowMemoize {
+
+  var cnt = 0
+
+  def incrCnt = { cnt+=1; cnt }
+
+  def render =
+    "div" #> SHtml.idMemoize(
+      memo =>
+        // display a bunch of items
+        "#count5" #> incrCnt &
+        // update the "two" div on button press
+        "button [onclick]" #> SHtml.ajaxInvoke(memo.setHtml _))
+}
+
